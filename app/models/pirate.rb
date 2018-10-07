6b027428efea47f120ad17c5e342822b778e2de7
Srija S <srijakumar7@gmail.com>
2018-10-07 18:43:01 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  pirates = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args [:weight]
+    @height = args [:height]
+    pirates << self
+  end
+
+  def self.all
+    pirates
+  end
+  
 end
