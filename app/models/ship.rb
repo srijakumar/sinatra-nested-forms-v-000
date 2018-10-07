5e7421205890e9479c51f1a87171342cea7f3126
Srija S <srijakumar7@gmail.com>
2018-10-07 18:45:26 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -7,8 +7,15 @@ class Ship
     @name = args[name]
     @type = args[type]
     @booty = args[booty]
-
     @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    ships = []
   end
 
 end
